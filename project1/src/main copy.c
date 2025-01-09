3df4233ffb4a539acc1c21ce0da9dd4d62279a2d
Lalis <laviniagtsantos15@gmail.com>
2025-01-09 19:26:41 +0100

    hf energy, mp2 correction, and read mo energies

--- project1/src/main copy.c
+++ project1/src/main copy.c
@@ -53,34 +53,14 @@ double *compute_one_electron_integrals(trexio_t *trexio_file, char *molecule);
  */
 void compute_two_electrons_integrals(trexio_t *trexio_file, char *molecule, int64_t *buffer_size, int32_t **index, double **value);
 
-int get_index(int i, int j, int k, int l, int mo_num);
 
-/**
- * @brief Computes the Hartree-Fock energy for a given molecule
- *
- * @param trexio_file [IN] the file where the data is stored
- * @param molecule [IN] the molecule considered
- * @param repulsion_energy [IN] nuclear repulsion energy of the molecule
- * @param one_e_integral [IN] one-electron integrals of the molecule
- * @param mo_num [IN] total number of molecular orbitals
- * @return Hartree-Fock energy
- */
 double compute_HF_energy(trexio_t *trexio_file, char *molecule, double repulsion_energy, double *one_e_integral, int mo_num);
 
-trexio_exit_code trexio_read_mo_energy(trexio_t *const file, double *const mo_energy);
+trexio_exit_code trexio_read_mo_energy(trexio_t* const file, double* const mo_energy);
 
-/**
- * @brief Computes the MP2 energy correction for a given molecule
- *
- * @param buffer_size [IN] the number of read integrals
- * @param index [IN] the indexes of the two-electron integrals
- * @param value [IN] the values of the two-electron integrals
- * @param mo_num [IN] total number of molecular orbitals
- * @param n_occ [IN] number of occupied molecular orbitals
- * @param orbital_energies [IN] orbital energy values
- * @return MP2 energy correction
- */
-double compute_MP2_energy(int64_t n_integrals, int32_t *index, double *value, int mo_num, int n_occ, double *orbital_energies);
+
+
+double compute_MP2_energy(int64_t n_integrals, int32_t* index, double* value, int mo_num, int n_occ, double* orbital_energies);
 
 /* PUBLIC FUNCTIONS DEFINITIONS */
 void main(int argc, char *argv[])
@@ -140,26 +120,22 @@ void main(int argc, char *argv[])
     // compute HF energy
     double hf_energy = compute_HF_energy(trexio_file, molecule, energy, data, mo_num);
 
+
     /* Read molecular orbital energies */
     double *mo_energy = malloc(mo_num * sizeof(double));
-    if (mo_energy == NULL)
-    {
+    if (mo_energy == NULL) {
         printf("Memory allocation error for mo_energy\n");
         exit(1);
     }
 
     rc = trexio_read_mo_energy(trexio_file, mo_energy);
-    if (rc != TREXIO_SUCCESS)
-    {
+    if (rc != TREXIO_SUCCESS) {
         printf("TREXIO Error reading molecular orbital energies:\n%s\n",
-               trexio_string_of_error(rc));
+            trexio_string_of_error(rc));
         free(mo_energy);
         exit(1);
-    }
-    else
-    {
-        for (int i = 0; i < mo_num; ++i)
-        {
+    } else {
+        for (int i = 0; i < mo_num; ++i) {
             // printf("Molecular orbital energy %d: %.5f\n", i, mo_energy[i]);
         }
     }
@@ -167,9 +143,9 @@ void main(int argc, char *argv[])
     double mp2_correction = compute_MP2_energy(buffer_size, index, value, mo_num, n_up, mo_energy);
     printf("MP2 correction: %.8f\n", mp2_correction);
 
-    // Compute total energy
     double total_energy = hf_energy + mp2_correction;
     printf("Total energy for molecule %s: %.8f\n", molecule, total_energy);
+
 
     /* Close the file */
     rc = trexio_close(trexio_file);
@@ -331,12 +307,11 @@ void compute_two_electrons_integrals(trexio_t *trexio_file, char *molecule, int6
     }
 }
 
+int get_index(int i, int j, int k, int l, int mo_num){
 
-int get_index(int i, int j, int k, int l, int mo_num)
-{
-// Calculate the index for accessing integrals
-    return i * mo_num * mo_num * mo_num + j * mo_num * mo_num + k * mo_num + l;
+    return i*mo_num*mo_num*mo_num + j*mo_num*mo_num + k*mo_num + l;
 }
+
 
 double compute_HF_energy(trexio_t *trexio_file, char *molecule, double repulsion_energy, double *one_e_integral, int mo_num)
 {
@@ -353,28 +328,34 @@ double compute_HF_energy(trexio_t *trexio_file, char *molecule, double repulsion
     // Determine the number of occupied orbitals
     mo_occ = compute_occupied_orbitals(trexio_file, molecule);
     // printf("Number of occupied orbitals: %d\n", mo_occ);
-
-    // Allocate memory for storing two-electron integral values
-    size_t max_size = (size_t)mo_occ * mo_occ * mo_occ * mo_occ;
-    double *two_e_value = (double *)calloc(max_size, sizeof(double));
-    if (two_e_value == NULL)
-    {
-        printf("Error: Fail to allocate memory for two_e_value.\n");
-        exit(1);
-    }
-
+    size_t max_size = (size_t) mo_occ * mo_occ * mo_occ * mo_occ;
+    double* mo_tei = (double*) calloc(max_size, sizeof(double));
     // Compute the sum of one-electron integrals for occupied orbitals
     for (i = 0; i < mo_occ; ++i)
     {
         sum_one_e += one_e_integral[i * mo_num + i];
     }
-    printf("Sum of one-electron integrals for molecule %s: %.8f\n sum*2: %.8f\n", molecule, sum_one_e, sum_one_e * 2);
+    printf("Sum of one-electron integrals for molecule %s: %.8f\n sum*2: %.8f\n", molecule, sum_one_e, sum_one_e*2);
 
     // Compute the sum of two-electron integrals for occupied orbitals
     compute_two_electrons_integrals(trexio_file, molecule, &buffer_size, &index, &value);
     // printf("Number of read two-electrons integrals for molecule %s: %ld\n", molecule, buffer_size);
 
-     // Store two-electron integrals in a structured format
+    float energy = 0;
+
+    /*// Store the two-electron integrals in the 1D array
+    // for (n = 0; n < buffer_size; ++n)
+    // {
+    //     i = index[4 * n + 0];
+    //     j = index[4 * n + 1];
+    //     k = index[4 * n + 2];
+    //     l = index[4 * n + 3];
+
+    //     V(i, j, k, l) = value[n];
+    //     printf("v[%d][%d][%d][%d]: %f\n", i, j, k, l, V(i, j, k, l));*/
+    // }
+
+    // Compute the energy contribution from two-electron integrals
     for (n = 0; n < buffer_size; ++n)
     {
         i = index[4 * n + 0];
@@ -383,44 +364,61 @@ double compute_HF_energy(trexio_t *trexio_file, char *molecule, double repulsion
         l = index[4 * n + 3];
         // printf("i: %d, j: %d, k: %d, l: %d\n", i, j, k, l);
 
+        if (mo_tei == NULL) {
+            printf("Error: Unable to allocate memory for mo_tei.\n");
+            exit(1);
+        }
+
         if (i < mo_occ && j < mo_occ && k < mo_occ && l < mo_occ)
         {
-            two_e_value[get_index(i, j, k, l, mo_num)] = value[n];
-            two_e_value[get_index(i, l, k, j, mo_num)] = value[n];
-            two_e_value[get_index(k, l, i, j, mo_num)] = value[n];
-            two_e_value[get_index(k, j, i, l, mo_num)] = value[n];
-            two_e_value[get_index(j, i, l, k, mo_num)] = value[n];
-            two_e_value[get_index(l, i, j, k, mo_num)] = value[n];
-            two_e_value[get_index(l, k, j, i, mo_num)] = value[n];
-            two_e_value[get_index(j, k, l, i, mo_num)] = value[n];
-        }
-    }
-    // Compute energy contribution from two-electron integrals
-    for (int i = 0; i < mo_occ; i++)
-    {
-        for (int j = 0; j < mo_occ; j++)
-        {
-            for (int k = 0; k < mo_occ; k++)
+            mo_tei[get_index(i, j, k, l, mo_num)] = value[n];
+            mo_tei[get_index(i, l, k, j, mo_num)] = value[n];
+            mo_tei[get_index(k, l, i, j, mo_num)] = value[n];
+            mo_tei[get_index(k, j, i, l, mo_num)] = value[n];
+            mo_tei[get_index(j, i, l, k, mo_num)] = value[n];
+            mo_tei[get_index(l, i, j, k, mo_num)] = value[n];
+            mo_tei[get_index(l, k, j, i, mo_num)] = value[n];
+            mo_tei[get_index(j, k, l, i, mo_num)] = value[n];
+
+            /*if (i == j && j == k && k == l)
             {
-                for (int l = 0; l < mo_occ; l++)
-                {
-                    if (i == k && j == l)
-                    {
-                        /*  printf("ijij i: %d, j: %d, k: %d, l: %d, value:%f \n", i, j, k, l, two_e_value[get_index(i, j, i, j, mo_num)]);
-                        //  printf("ijji i: %d, j: %d, k: %d, l: %d, value:%f \n", i, j, j, i, two_e_value[get_index(i, j, j, i, mo_num)]);*/
-                        double ijij = two_e_value[get_index(i, j, i, j, mo_num)];
-                        double ijji = two_e_value[get_index(i, j, j, i, mo_num)];
-                        sum_two_e += 2 * ijij - ijji;
-                    }
+                // energy += 2 * value[n] - value[n];
+                energy += value[n];
+            }
+            else if (i == k && j == l && i != j)
+            {
+                // energy += 4 * (2 * value[n] - value[n]);
+                energy += 4 * value[n];
+            }
+            else if ((i == j && l == k) || (i == l && j == k))
+            {
+                energy -= 2 * value[n];
+            }*/
+        }
+
+    }
+        for (int i = 0; i < mo_occ; i++) { // occupied orbitals
+        for (int j = 0; j < mo_occ; j++) { // occupied orbitals
+            for (int k = 0; k < mo_occ; k++) { // virtual orbitals
+                for (int l = 0; l < mo_occ; l++) { // virtual orbitals
+                if (i == k && j == l) {
+                    //
+                //  printf("ijij i: %d, j: %d, k: %d, l: %d, value:%f \n", i, j, k, l, mo_tei[get_index(i, j, i, j, mo_num)]);
+                //  printf("ijji i: %d, j: %d, k: %d, l: %d, value:%f \n", i, j, j, i, mo_tei[get_index(i, j, j, i, mo_num)]);
+                    double ijij = mo_tei[get_index(i, j, i, j, mo_num)];
+                    double ijji = mo_tei[get_index(i, j, j, i, mo_num)];
+                    sum_two_e += 2*ijij - ijji;
+                }
                 }
             }
         }
     }
-    free(two_e_value);
 
-    printf("Sum of two-electron integrals for molecule %s: %.7f\n", molecule, sum_two_e);
+    printf("Sum of two-electron integrals for molecule %s: %.8f\n", molecule, sum_two_e);
+    
 
-    // Compute Hartree-Fock energy
+
+    // Step 4: Compute Hartree-Fock energy
     hf_energy = repulsion_energy + 2 * sum_one_e + sum_two_e;
 
     // Output the computed HF energy
@@ -429,68 +427,64 @@ double compute_HF_energy(trexio_t *trexio_file, char *molecule, double repulsion
     return hf_energy;
 }
 
-double compute_MP2_energy(int64_t buffer_size, int32_t *index, double *value, int mo_num, int n_occ, double *orbital_energies)
-{
 
-    double mp2_correction = 0.0;
 
-    // Allocate memory for storing two-electron integral values
-    size_t max_size = (size_t)mo_num * mo_num * mo_num * mo_num;
-    double *two_e_value = (double *)calloc(max_size, sizeof(double));
-    if (two_e_value == NULL)
-    {
-        printf("Error: Fail to allocate memory for two_e_value.\n");
+
+double compute_MP2_energy(int64_t buffer_size, int32_t* index, double* value, int mo_num, int n_occ, double* orbital_energies) {
+
+    double MP2_energy = 0.0;
+    
+
+    size_t max_size = (size_t) mo_num * mo_num * mo_num * mo_num;
+    double* mo_tei = (double*) calloc(max_size, sizeof(double));
+    if (mo_tei == NULL) {
+        printf("Error: Unable to allocate memory for mo_tei.\n");
         exit(1);
     }
+    
 
-    // Store MP2-relevant two-electron integrals
-    for (int n = 0; n < buffer_size; ++n)
-    {
-        int a = index[4 * n + 0];
-        int b = index[4 * n + 1];
-        int i = index[4 * n + 2];
-        int j = index[4 * n + 3];
+    for (int n = 0; n < buffer_size; ++n) {
+        int i = index[4*n + 0];
+        int j = index[4*n + 1];
+        int k = index[4*n + 2];
+        int l = index[4*n + 3];
 
-        // Filter for MP2 integrals < a b | i j >
-        if (a >= n_occ || b >= n_occ || i < n_occ || j < n_occ)
-        {
-            two_e_value[get_index(a, b, i, j, mo_num)] = value[n];
-            two_e_value[get_index(a, j, i, b, mo_num)] = value[n];
-            two_e_value[get_index(i, j, a, b, mo_num)] = value[n];
-            two_e_value[get_index(i, b, a, i, mo_num)] = value[n];
-            two_e_value[get_index(b, a, j, i, mo_num)] = value[n];
-            two_e_value[get_index(j, a, b, i, mo_num)] = value[n];
-            two_e_value[get_index(j, i, b, a, mo_num)] = value[n];
-            two_e_value[get_index(b, i, j, a, mo_num)] = value[n];
+        // Filter only MP2 relevant integrals < a b | i j >
+        if (i < n_occ || j < n_occ || k >= n_occ || l >= n_occ) {
+        continue; 
         }
-        else
-        {
-            continue;
-        }
+        mo_tei[get_index(i, j, k, l, mo_num)] = value[n];
+        mo_tei[get_index(i, l, k, j, mo_num)] = value[n];
+        mo_tei[get_index(k, l, i, j, mo_num)] = value[n];
+        mo_tei[get_index(k, j, i, l, mo_num)] = value[n];
+        mo_tei[get_index(j, i, l, k, mo_num)] = value[n];
+        mo_tei[get_index(l, i, j, k, mo_num)] = value[n];
+        mo_tei[get_index(l, k, j, i, mo_num)] = value[n];
+        mo_tei[get_index(j, k, l, i, mo_num)] = value[n];
+
     }
 
-    // Compute MP2 energy correction
-    for (int i = 0; i < n_occ; i++)
-    {
-        for (int j = 0; j < n_occ; j++)
-        {
-            for (int a = n_occ; a < mo_num; a++)
-            {
-                for (int b = n_occ; b < mo_num; b++)
-                {
-                    double e_i = orbital_energies[i];
-                    double e_j = orbital_energies[j];
-                    double e_a = orbital_energies[a];
-                    double e_b = orbital_energies[b];
-                    double ijab = two_e_value[get_index(a, b, i, j, mo_num)];
-                    double ijba = two_e_value[get_index(b, a, i, j, mo_num)];
-                    double denominator = e_i + e_j - e_a - e_b;
-                    double numerator = ijab * (2 * ijab - ijba);
-                    mp2_correction += numerator / denominator;
+    for (int i = 0; i < n_occ; i++) { // occupied orbitals
+        for (int j = 0; j < n_occ; j++) { // occupied orbitals
+            for (int a = n_occ; a < mo_num; a++) { // virtual orbitals
+                for (int b = n_occ; b < mo_num; b++) { // virtual orbitals
+                    double coulomb = mo_tei[get_index(a, b, i, j, mo_num)];
+                    double exchange = mo_tei[get_index(b, a, i, j, mo_num)];
+                    double denominator = orbital_energies[i] + orbital_energies[j] - orbital_energies[a] - orbital_energies[b];
+                    double numerator = coulomb * (2*coulomb - exchange);
+                    MP2_energy += numerator / denominator;
                 }
             }
         }
     }
-    free(two_e_value);
-    return mp2_correction;
+    free(mo_tei);
+    return MP2_energy;
 }
+
+
+
+
+
+
+
+
